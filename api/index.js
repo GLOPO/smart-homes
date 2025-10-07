c2886006e26f5886afc31f4b5eb880a5aa6fe728
GLOPO <gloryohuali@gmail.com>
2025-10-07 16:17:11 +0100

    create a middleware and a function to habdle possible errors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,17 @@ app.use('/api/users', userRouter);
 
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})
+
 
 mongoose
   .connect(process.env.MONGO)
