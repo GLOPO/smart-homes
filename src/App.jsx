2a09c647c5b83eb2b59f6b6d91219d4e8bf48aec
GLOPO <gloryohuali@gmail.com>
2025-10-07 02:22:08 +0100

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from './pages/Signin'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
