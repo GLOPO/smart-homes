8fd3bf182d7265c049bdbe3d791e5a4f0c16da0b
GLOPO <gloryohuali@gmail.com>
2025-10-09 16:20:38 +0100

    Create redux toolkit and implimented into the sign in page

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         secure: false
       }
     }
+  },
+  watch: {
+    usePolling: true,
+    interval: 100
   },
   plugins: [react()],
 })
