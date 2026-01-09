b9a3b7edeb542824c7c0eaee4899c69cd4be2694
GLOPO <gloryohuali@gmail.com>
2026-01-09 06:53:46 +0100

    orign: true

--- server/api/index.js
+++ server/api/index.js
@@ -32,16 +32,7 @@ const allowedOrigins = [
 
 // 2. Configure CORS
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or Postman)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
